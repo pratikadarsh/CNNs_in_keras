0cbbb6e837cf281c2e9cc52aa302a9571f33328c
Pratik Adarsh <pratikadarsh24@gmail.com>
2020-12-10 22:38:16 +0530

    move model creation into model.py and enable dynamic model selection.

---
+++ model.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import keras
+from keras import Model
+from keras.applications.vgg16 import VGG16
+from keras.layers import Dense, GlobalAveragePooling2D
+
+
+
+class CnnModel():
+    def __init__(self, model='vgg16', num_classes=10):
+        pass
+
+    def get_model(self, model_name, num_classes=10):
+        model_func = getattr(self, model_name, None)
+        if model_func is None:
+            print("Warning !!! Model could not be retrieved.")
+            return None
+        else:
+            return model_func(num_classes)
+
+    def vgg16(self, num_classes=10):
+        base_model = VGG16(include_top=False, input_shape=(32,32,3))
+        base_model.trainable = False
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(128)(x)
+        x = Dense(32)(x)
+        logits = Dense(10, activation='softmax')(x) #83511963
+        model = Model(inputs=base_model.input, outputs=logits)
+        return model
+
+
--- train.py
+++ train.py
@@ -2,10 +2,12 @@ import keras
 import tensorflow as tf
 import numpy as np
 
-from keras import Model, utils
+from keras import utils
 from keras.datasets import cifar10
 from keras.applications.vgg16 import VGG16
 from keras.layers import Flatten, Dense, Input, GlobalAveragePooling2D
+
+from model import CnnModel
 
 # Load Data.
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -13,14 +15,7 @@ y_train = utils.to_categorical(y_train, 10)
 y_test = utils.to_categorical(y_test, 10)
 
 # Load and Compile Model.
-base_model = VGG16(include_top=False, input_shape=(32,32,3))
-base_model.trainable = False
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(128)(x)
-x = Dense(32)(x)
-logits = Dense(10, activation='softmax')(x) #83511963
-model = Model(inputs=base_model.input, outputs=logits)
+model = CnnModel().get_model('vgg16')
 #model.summary()
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
